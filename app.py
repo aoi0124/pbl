3804945143668fdd457ca04a1c9ebee6f8558c6d
aoi0124 <aoi.era.5t@stu.hosei.ac.jp>
2023-10-09 15:06:37 +0900

    flask

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html',  title="バスケについての投稿",  body="投稿：",  bodylist="投稿一覧")
 
+@app.route('/result', methods=['POST'])
+def result():
+	text=request.form['comment']
+	return "<pre>"+text+"</pre>"
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='localhost')
