acec283f5671eb267766a2e153057dc2d00dc46d
aoi0124 <aoi.era.5t@stu.hosei.ac.jp>
2023-11-14 01:18:32 +0900

    apiの作成

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 app=Flask(__name__,static_folder='./templates/static')
 app=Flask(__name__,static_folder="templates", static_url_path='')
+# 仮
+posts = {
+    123: {"post_id": 123, "content": "投稿の内容", "comments": [1, 2, 3]},
+    # 他の投稿...
+}
 
+comments = {
+    1: {"comment_id": 1, "post_id": 123, "text": "コメント1の内容"},
+    2: {"comment_id": 2, "post_id": 123, "text": "コメント2の内容"},
+    3: {"comment_id": 3, "post_id": 123, "text": "コメント3の内容"},
+}
+
+@app.route('/api/posts/<int:post_id>/comments/count', methods=['GET'])
+def get_comment_count(post_id):
+    post = posts.get(post_id)
+
+    if post:
+        comment_count = len(post.get("comments", []))
+        return jsonify({"post_id": post_id, "comment_count": comment_count})
+    else:
+        return jsonify({"error": "Post not found"}), 404
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html',  title="バスケについての投稿",  body="投稿：",  bodylist="投稿一覧")
